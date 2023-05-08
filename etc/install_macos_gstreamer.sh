e6e3f797ff172243e51d58fddfa1075a57e905f8
Mukilan Thiyagarajan <me@mukilan.in>
2023-05-08 12:50:48 +0530

    fix curl command

--- etc/install_macos_gstreamer.sh
+++ etc/install_macos_gstreamer.sh
@@ -1,10 +1,10 @@
-#!/bin/env bash
+#!/bin/sh
 
-VERSION=1.22.1
+VERSION=1.22.2
 URL_BASE=https://github.com/servo/servo-build-deps/releases/download/macOS
 
 cd /tmp
-curl "${URL_BASE}/gstreamer-1.0-${VERSION}-universal.pkg" -o gstreamer.pkg
-curl "${URL_BASE}/gstreamer-1.0-devel-${VERSION}-universal.pkg" -o gstreamer-dev.pkg
+curl -L "${URL_BASE}/gstreamer-1.0-${VERSION}-universal.pkg" -o gstreamer.pkg
+curl -L "${URL_BASE}/gstreamer-1.0-devel-${VERSION}-universal.pkg" -o gstreamer-dev.pkg
 sudo installer -pkg 'gstreamer.pkg' -target /
 sudo installer -pkg 'gstreamer-dev.pkg' -target /
